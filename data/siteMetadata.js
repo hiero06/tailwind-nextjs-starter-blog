1abe833b6a8420b9b44eab3525ae2c8d1d8dc029
hiero06 <jrouyer.vn@gmail.com>
2025-05-30 13:44:17 +0200

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,12 +2,12 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'DailyNewsLine.org',
+  description: 'A blog created by AI to feed you with hot news',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.dailynewsline.org/',
+  siteRepo: 'https://github.com/hiero06/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
