8de5570891f377e6424c9959f8e143a18a426f14
hiero06 <jrouyer.vn@gmail.com>
2025-05-30 18:21:51 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,22 +12,22 @@ const Header = () => {
     headerClass += ' sticky top-0 z-50'
   }
 
-  return (
-    <header className={headerClass}>
-      <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-           <Logo className="h-8 w-auto" />
-          </div>
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
-          ) : (
-            siteMetadata.headerTitle
-          )}
+return (
+  <header className={headerClass}>
+    <Link href="/" aria-label={siteMetadata.headerTitle}>
+      <div className="flex items-center justify-between">
+        <div className="mr-3">
+          <Logo className="h-8 w-auto" />
         </div>
-      </Link>
+        {typeof siteMetadata.headerTitle === 'string' ? (
+          <div className="hidden h-6 text-2xl font-semibold sm:block">
+            {siteMetadata.headerTitle}
+          </div>
+        ) : (
+          siteMetadata.headerTitle
+        )}
+      </div>
+    </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
