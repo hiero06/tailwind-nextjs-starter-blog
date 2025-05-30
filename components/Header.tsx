4422eeb688d928c800e0d712a3c36285d8fe79c1
hiero06 <jrouyer.vn@gmail.com>
2025-05-30 18:26:33 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,12 +12,12 @@ const Header = () => {
     headerClass += ' sticky top-0 z-50'
   }
 
-return (
+  return (
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-           <Logo className="h-8 w-auto" />
+            <Logo />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
@@ -26,8 +26,8 @@ return (
           ) : (
             siteMetadata.headerTitle
           )}
-       </div>
-    </Link>
+        </div>
+      </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
